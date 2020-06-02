6d665ca50cf651e365cf2431e10b77baed1e84f6
colombelli <fcolombelli@inf.ufrgs.br>
2020-06-02 10:53:24 -0300

    fingerprint authentication POC implemented but can suit our needs

--- android/app/src/main/kotlin/com/pgcheck/pg_check/MainActivity.kt
+++ android/app/src/main/kotlin/com/pgcheck/pg_check/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.pgcheck.pg_check
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,77 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:pg_check/app_localizations.dart';
 import 'package:pg_check/services/auth.dart';
 
-class Home extends StatelessWidget {
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
 
   final AuthService _auth = AuthService();
+
+  LocalAuthentication bioauth = LocalAuthentication();
+  Future<void> _checkBiometrics() async {
+     bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await bioauth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    }); 
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await bioauth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    }); 
+  } 
+
+  Future<void> _bioauthenticate() async {
+    bool bioauthenticated = false;
+
+    try {
+      bioauthenticated = await bioauth.authenticateWithBiometrics(
+        localizedReason: "Scan your fingerprint to authenticate",
+        useErrorDialogs: true,
+        stickyAuth: false,
+
+      );
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _authorized = bioauthenticated ? "Authorized" : "Not authorized";
+    });
+
+  }
+
+  bool _canCheckBiometrics;
+  List<BiometricType> _availableBiometrics;
+  String _authorized = "Not authorized"; //temporary to proof-of-concept
+  
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +97,29 @@ class Home extends StatelessWidget {
           )
         ]
       ),
+    body: Container(
+      padding: EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(translation('can_check_biometrics') + "$_canCheckBiometrics\n"),
+            RaisedButton(
+              child: Text(translation('check_biometrics')),
+              onPressed: _checkBiometrics),
+            Text(translation('available_biometrics') + "$_availableBiometrics\n"),
+            RaisedButton(
+              child: Text(translation('get_available_biometrics')),
+              onPressed: _getAvailableBiometrics),
+            Text(translation('current_state') + "$_authorized\n"),
+            RaisedButton(
+              child: Text(translation('bioauthenticate')),
+              onPressed: _bioauthenticate),
+          ],
+          )
+      ),
+    ),
     );
   }
 }
+
