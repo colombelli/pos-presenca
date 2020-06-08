bf3dc7b9bfd49372e3fe6c2a7a18f7ffc56ff9fa
Bernardo Hummes <hummescmpa@gmail.com>
2020-06-08 13:03:27 -0300

    started professors menu

---
+++ lib/screens/professor/professor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Professor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final translation = (String s) => AppLocalizations.of(context).translate(s);
+
+    return Scaffold(
+      
+    );
+  }
+}
