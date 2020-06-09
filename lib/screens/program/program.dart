92cfc95e0ba24f1bb63eedf924f9023f6ad70b22
Bernardo Hummes <hummescmpa@gmail.com>
2020-06-09 13:29:38 -0300

    added logout for programs menu

--- lib/screens/program/program.dart
+++ lib/screens/program/program.dart
@@ -1,10 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:pg_check/app_localizations.dart';
+import 'package:pg_check/services/auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
 class Program extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
 
@@ -14,6 +17,19 @@ class Program extends StatelessWidget {
       appBar: new AppBar(
         leading: Icon(Icons.account_balance),
         title: new Text("PPGC Menu"),
+        backgroundColor: Colors.blue[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            onPressed: () async {
+              await _auth.signOut();  // and does nothing else because there's a streaming 
+                                      // already hearing the value of User and when it is null
+                                      // it renders the Authenticate screen instead of Home
+            },
+            label: Text(translation('logout_button')),
+          )
+        ]
       ),
       body: MenuList(),
     );
