3f068142cb842bc9e26090f3839f5246e3610dc6
colombelli <fcolombelli@inf.ufrgs.br>
2020-06-15 13:23:14 -0300

    shadowed name error fixed; returning the right student program

--- cloud_functions/functions/src/index.ts
+++ cloud_functions/functions/src/index.ts
@@ -6,26 +6,37 @@ admin.initializeApp(functions.config().firebase)
 
 export const presenceRegistration = functions.https.onRequest((request, response) => {
   
-    let regCode = request.query.regCode
+    //let regCode = request.query.regCode
     let userID = request.query.userID
 
-    let codeStr = String(regCode)
+    //let codeStr = String(regCode)
     let userIDstr = String(userID)
 
     admin.firestore().collection("users").doc(userIDstr).get()
         .then(snapshot => {  
             
-            let userData = snapshot.data()
+            const userData = snapshot.data()
             
             if (userData){
-                response.send(userData)
+                
+                const program = String(userData.program)
+                admin.firestore().collection("programs").where("name", "==", program).get()
+                    .then(progsSnapshot => {
+                        
+                        if (progsSnapshot.docs){
+                            const programID = progsSnapshot.docs[0].id
+                            response.send(programID)
+                        }
+
+                    })
+                    .catch(err => {
+                        response.send('Error:'+err)
+                    })
+
+
             } else {
                 response.send("Error: couldn't find user")
             }
-            console.log(codeStr)
-            console.log(userData)
-
-            
 
         })
         .catch(err => {
